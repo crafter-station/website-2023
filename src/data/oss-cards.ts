75001266612b7cbd31195135ea7f130d0f2731b1
EdwardR0507 <ramosedward21@gmail.com>
2023-09-22 17:30:06 -0500

    feat: oss page and refactor components

---
+++ src/data/oss-cards.ts
@@ -0,0 +1,24 @@
+export const ossCardsData = [
+  {
+    title: "Memora Date",
+    description: "Yet Another Reminder App",
+    body: "First project, big step. Memora Date offers a simple list of reminders, one-time or recurring events, and the ability to import contacts. Email alerts keep you on track.",
+    link_demo: "https://memora-date.vercel.app/",
+    link_repository: "https://github.com/Railly/memora-date",
+  },
+  {
+    title: "One Hunter Theme",
+    description: "Popular VSCode Theme",
+    body: "Merging the best of Vercel Theme ▲ and One Dark Pro 🎨, One Hunter brings a unique aesthetic to your coding environment.",
+    link_demo:
+      "https://marketplace.visualstudio.com/items?itemName=RaillyHugo.one-hunter",
+    link_repository: "https://github.com/Railly/one-hunter-vscode",
+  },
+  {
+    title: "Spotigen",
+    description: "ChatGPT Plugin",
+    body: "Simply type in what you're feeling or looking for, and Spotigen will generate a Spotify playlist to match. A straightforward way to discover new music.",
+    link_demo: "https://spotigen.vercel.app/",
+    link_repository: "https://github.com/Railly/spotigen-chat-gpt-plugin",
+  },
+];
