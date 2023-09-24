aa6b134b98b4e307d965a5c85653b1c82d4bc0f3
Railly <railly_2009@hotmail.com>
2023-09-23 19:06:05 -0500

    feat: setup linter, formatter & CI/CD pipeline

---
+++ .prettierrc.cjs
@@ -0,0 +1,15 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+   ...require('prettier-config-standard'),
+  plugins: [require.resolve('prettier-plugin-astro')],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+        jsxSingleQuote: false,
+      }
+    }
+  ],
+  singleQuote: false,
+}
