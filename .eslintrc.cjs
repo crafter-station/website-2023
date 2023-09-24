9407e25976089c72c76d4d2f1a25f8cbdbeee7d7
Railly Hugo <51397083+Railly@users.noreply.github.com>
2023-09-23 19:14:15 -0500

    feat: setup linter, formatter & CI/CD pipeline (#4)
    * feat: setup linter, formatter & CI/CD pipeline
    
    * fix: lint all project

---
+++ .eslintrc.cjs
@@ -0,0 +1,40 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path")
+/** @type {import("eslint").Linter.Config} */
+
+module.exports = {
+  extends: [
+    "standard-with-typescript",
+    "plugin:prettier/recommended",
+    "plugin:astro/recommended"
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    tsconfigRootDir: path.normalize(__dirname),
+    sourceType: "module",
+    ecmaVersion: "latest"
+  },
+  rules: {
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+    "@typescript-eslint/no-var-requires": "error",
+    "@typescript-eslint/triple-slash-reference": "off",
+    "@typescript-eslint/naming-convention": "off",
+    "@typescript-eslint/strict-boolean-expressions": "off"
+  },
+  overrides: [
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"]
+      },
+      rules: {
+        // Override or add rule settings here, such as:
+        // "astro/no-set-html-directive": "error"
+      }
+    }
+  ]
+}
